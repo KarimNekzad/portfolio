4839aff6e26caf7983f7e2796682e440c29f90a1
Karim Nekzad <karimnekzad00@gmail.com>
2025-05-12 19:44:35 -0400

    Meta tags for iOS and favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,31 @@
+import { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
+
+export const metadata: Metadata = {
+  title: 'Karim Nekzad • Portfolio',
+  icons: {
+    icon: [
+      { url: '/favicons/favicon.ico' },
+      { url: '/favicons/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
+    ],
+    apple: [{ url: '/favicons/apple-touch-icon.png', sizes: '180x180' }],
+    other: [
+      {
+        rel: 'mask-icon',
+        url: '/favicons/safari-pinned-tab.svg',
+        color: 'oklch(0.646 0.222 41.116)',
+      },
+      { rel: 'shortcut icon', url: '/favicons/favicon.ico' },
+    ],
+  },
+  manifest: '/favicons/site.webmanifest',
+  themeColor: 'oklch(0.646 0.222 41.116)',
+  appleWebApp: {
+    capable: true,
+    statusBarStyle: 'black-translucent',
+  },
+};
 
 const inter = Inter({ subsets: ['latin'] });
 
